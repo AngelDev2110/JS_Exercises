de67364b00a066fc5ac33457f09f6a3643bcffd7
Angel DLT <angeldev2110@gmail.com>
2023-09-16 20:52:25 -0600

    Anagram function

--- Anagram.js
+++ Anagram.js
@@ -6,3 +6,16 @@
  * - NO hace falta comprobar que ambas palabras existan.
  * - Dos palabras exactamente iguales no son anagrama.
  */
+
+function anagram(word1,word2){
+    if (word1 == word2)
+        return false
+    else if (word1.length != word2.length)
+        return false
+    else{
+        word1Sorted = word1.split('').sort().join('')
+        word2Sorted = word2.split('').sort().join('')
+        return word1Sorted == word2Sorted
+    }
+}
+console.log(anagram('listen','silent'))
