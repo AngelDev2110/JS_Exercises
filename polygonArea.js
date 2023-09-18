8c12aff28053fe2036ee81fec33e1cb41b46ecf4
Angel DLT <angeldev2110@gmail.com>
2023-09-17 21:17:21 -0600

    polygonArea clases and function

--- polygonArea.js
+++ polygonArea.js
@@ -6,3 +6,43 @@
  * - Imprime el cálculo del área de un polígono de cada tipo.
  */
 
+function area(pol){
+    console.log(pol.calcArea())
+}
+
+class Triangle{
+    constructor(base,height){
+        this.base = base
+        this.height = height
+    }
+    calcArea(){
+        return((this.base*this.height)/2)
+    }
+}
+
+class Square{
+    constructor(side){
+        this.side = side
+    }
+    calcArea(){
+        return(this.side*this.side)
+    }
+}
+
+class Rectangle{
+    constructor(side1,side2){
+        this.side1 = side1
+        this.side2 = side2
+    }
+    calcArea(){
+        return(this.side1*this.side2)
+    }
+}
+
+triangle = new Triangle(4,6)
+square = new Square(4)
+rectangle = new Rectangle(4,8)
+
+area(triangle)
+area(square)
+area(rectangle)
