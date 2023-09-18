9fd6fcdb398ae24d816f197ee8e725b1f562c9f5
Angel DLT <angeldev2110@gmail.com>
2023-09-17 21:04:18 -0600

    primeNumber Function and cycle

--- primeNumber.js
+++ primeNumber.js
@@ -2,3 +2,12 @@
  * Escribe un programa que se encargue de comprobar si un número es o no primo.
  * Hecho esto, imprime los números primos entre 1 y 100.
  */
+
+function primeNumber(num){
+    if(num%2!=0 && num%3!=0 && num%5!=0 && num !=1 || num == 2 || num == 3 || num == 5){
+        console.log(num)
+    }
+}
+for(let i = 1; i <= 100; i++){
+    primeNumber(i)
+}
